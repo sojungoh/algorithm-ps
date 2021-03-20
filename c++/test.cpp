a20bbf593735a800847b6e13d2aab839f386dc69
sojungoh <sojung3514@gmail.com>
2021-03-21 02:30:40 +0900

    c/c++ 컴파일 환경설정

---
+++ c++/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int number;
+
+	cin >> number;
+
+	printf("%d", number);
+	printf("%d", number);
+	printf("%d", number);
+	
+	return 0;
+};
