d7d7fcb28ca9d974ae6820fa0818bf66c698ac1a
sojungoh <sojung3514@gmail.com>
2021-04-05 23:42:53 +0900

    c++ commit

--- c++/test.cpp
+++ c++/test.cpp
@@ -5,11 +5,16 @@ using namespace std;
 int main(void) {
 	int number;
 
-	cin >> number;
+	//숫자 입력
+	//1차원 배열 입력
+	//2차원 배열 입력
+	//문자 입력
+	//문자열 입력
+	
 
-	printf("%d", number);
-	printf("%d", number);
-	printf("%d", number);
+	cout << "This " << "is a " << "single C++ statement";
+
+	cin >> number;
 	
 	return 0;
 };
