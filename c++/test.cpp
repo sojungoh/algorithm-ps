6c360ce8a5c6dd3a5f06e513da6b6cbc9e8d16be
sojungoh <sojung3514@gmail.com>
2021-04-06 23:57:53 +0900

    c++ 2021.04.06

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,19 +2,25 @@
 
 using namespace std;
 
-int main(void) {
+int change_val(int &p){
+	p = 3;
+
+	return 0;
+}
+
+int main(){
 	int number;
+	int arr[3] = {1, 2, 3};
 
-	//숫자 입력
-	//1차원 배열 입력
-	//2차원 배열 입력
-	//문자 입력
-	//문자열 입력
-	
+	cout << arr[0] << arr[1] << arr[2] << endl;
 
-	cout << "This " << "is a " << "single C++ statement";
+	int(&ref)[3] = arr;
+
+	ref[0] = 2;
+	ref[1] = 3;
+	ref[2] = 1;
+
+	cout << arr[0] << arr[1] << arr[2] << endl;
 
 	cin >> number;
-	
-	return 0;
-};
+}
