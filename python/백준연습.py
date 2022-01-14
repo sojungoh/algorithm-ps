ebbb296c19c81db514ef53d9d2ff54bc1ca56023
sojungoh <sojung3514@gmail.com>
2022-01-14 23:39:30 +0900

    22/01/14 python

--- python/백준연습.py
+++ python/백준연습.py
@@ -1,25 +1,4 @@
-class math:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        
-    def add(self):
-        print(self.x + self.y)
-        
-    def sub(self):
-        print(self.x - self.y)
-    
-    def mul(self):
-        print(self.x * self.y)
-        
-    def div(self):
-        print(self.x // self.y)
-        
-    def rem(self):
-        print(self.x % self.y)    
+x = int(input())
+y = input()
 
-x, y = map(int, input().split())
-
-test = math(x, y)
-
-test.add(); test.sub(); test.mul(); test.div(); test.rem()
+print(x*int(y[2]), x*int(y[1]), x*int(y[0]), x*int(y), sep='\n')
