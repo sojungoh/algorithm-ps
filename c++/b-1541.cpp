e58571935b6d5a46f1111ec4247b152cad13967c
sojungoh <sojung3514@gmail.com>
2022-07-13 00:31:30 +0900

    solved[s2]: 백준 1541 잃어버린 괄호

--- c++/b-1541.cpp
+++ c++/b-1541.cpp
@@ -1,45 +1,42 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <string>
 
 using namespace std;
 
-char a[51], n = 0, ans = 0;
+string s;
 
 void solve() {
-	size_t sz = 0;
+	size_t idx;
+	int ans = stoi(s, &idx);
+	int sub = 0;
 
-	int k = 0, t = 0;
+	for (int i = idx; i < s.size(); i++) {
+		if (s[i] != '+' && s[i] != '-')
+			continue;
 
-	for (int i = 0; i < sizeof(a); i++) {
-
-		if (a[i] == '-' || a[i] == '+') {
-			if (a[sz] == '-') {
-				k++;
-				if (k % 2) {
-
-				}
-				n = -1 * stoi(&a[sz], &sz);
-			}
-			else
-				n += stoi(&a[sz], &sz);
+		if (sub == 0) {
+			if (s[i] == '+')
+				ans += stoi(&s[i + 1]);
+			else if (s[i] == '-')
+				sub = stoi(&s[i + 1]);
+		}
+		else {
+			sub += stoi(&s[i + 1]);
 		}
 	}
+	ans -= sub;
 
+	cout << ans << '\n';
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	int i = 0;
+	cin >> s;
 
-	do {
-		cin >> a[i];
-		++i;
-	} while (getc(stdin) == ' ');
-
+	solve();
 
 	return 0;
 }
