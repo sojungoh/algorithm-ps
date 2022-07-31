9b627d22bebfd581de0f03e62595e0343c911ffd
sojungoh <sojung3514@gmail.com>
2022-08-01 01:01:23 +0900

    solved[g1]: 백준 13460 구슬 탈출 2

--- c++/b-13460.cpp
+++ c++/b-13460.cpp
@@ -6,97 +6,79 @@ using namespace std;
 
 int n, m, ans = -1;
 char map[10][10];
-queue<pair<int, int>> q_red;
-queue<pair<int, int>> q_blue;
+queue<pair<int, int>> q;
 
 void solve() {
 	bool goal_flag = false;
-	int red_visit[10][10] = { 0, }, blue_visit[10][10] = { 0, };
-	int dist[10][10] = { 0, }, dx[4] = { -1, 0, 1, 0 }, dy[4] = { 0, -1, 0, 1 };
+	int turn = 1, dx[4] = { -1, 0, 1, 0 }, dy[4] = { 0, -1, 0, 1 };
 
-	int turn = 0;
+	while (!q.empty()) {
+		int q_sz = q.size();
 
-	red_visit[q_red.front().first][q_red.front().second] = 1;
-	blue_visit[q_blue.front().first][q_blue.front().second] = 1;
+		while (q_sz) {
+			int direction = q.front().first;
+			int curr_red[2] = { q.front().second / 1000, (q.front().second % 1000) / 100 };
+			int curr_blue[2] = { (q.front().second % 100) / 10, q.front().second % 10 };
+			q.pop();
 
-	while (!q_red.empty() && !q_blue.empty()) {
-		int curr_red[2] = { q_red.front().first, q_red.front().second };
-		q_red.pop();
-		int curr_blue[2] = { q_blue.front().first, q_blue.front().second };
-		q_blue.pop();
+			//cout << "red: " << curr_red[0] << ' ' << curr_red[1] << '\n';
+			//cout << "blue: " << curr_blue[0] << ' ' << curr_blue[1] << '\n';
 
-		//if (dist[curr_red[0]][curr_red[1]] > 20) break;
-		//if (turn > 10) break;
+			if (turn > 10) break;
 
-		cout << "red: " << curr_red[0] << ' ' << curr_red[1] << '\n';
-		cout << "blue: " << curr_blue[0] << ' ' << curr_blue[1] << '\n';
+			for (int i = 0; i < 4; i++) {
+				int red_x = curr_red[0], red_y = curr_red[1];
+				int blue_x = curr_blue[0], blue_y = curr_blue[1];
 
-		for (int i = 0; i < 4; i++) {
-			int red_x = curr_red[0], red_y = curr_red[1];
-			int blue_x = curr_blue[0], blue_y = curr_blue[1];
+				if (direction != -1 && (direction + i) % 2 == 0) continue;
 
-			if (map[red_x + dx[i]][red_y + dy[i]] == '#') continue;
+				bool red_flag = true, blue_flag = true;
 
-			bool red_flag = true, blue_flag = true;
+				while (red_flag || blue_flag) {
+					red_flag = false, blue_flag = false;
 
-			while (red_flag || blue_flag) {
-				red_flag = false, blue_flag = false;
-
-				if (map[red_x + dx[i]][red_y + dy[i]] == '.') {
-					red_x += dx[i], red_y += dy[i];
-					//visited[red_x][red_y] = 1;
-					red_flag = true;
-				}
-				if (map[blue_x + dx[i]][blue_y + dy[i]] == '.') {
-					blue_x += dx[i], blue_y += dy[i];
-					blue_flag = true;
-				}
-
-				if (map[blue_x + dx[i]][blue_y + dy[i]] == 'O') break;
-
-				//if (map[red_x + dx[i]][red_y + dy[i]] == 'O')
-				//	goal_flag = true;
-
-				if (red_x == blue_x && red_y == blue_y) {
-					if (red_flag) {
-						//visited[red_x][red_y] = 0;
-						red_x -= dx[i], red_y -= dy[i];
-						red_flag = false;
+					if (map[red_x + dx[i]][red_y + dy[i]] == '.') {
+						red_x += dx[i], red_y += dy[i];
+						red_flag = true;
 					}
-					else if (blue_flag) {
-						blue_x -= dx[i], blue_y -= dy[i];
-						blue_flag = false;
+					if (map[blue_x + dx[i]][blue_y + dy[i]] == '.') {
+						blue_x += dx[i], blue_y += dy[i];
+						blue_flag = true;
+					}
+
+					if (map[blue_x + dx[i]][blue_y + dy[i]] == 'O') break;
+
+					if (red_x == blue_x && red_y == blue_y) {
+						if (red_flag) {
+							red_x -= dx[i], red_y -= dy[i];
+							red_flag = false;
+						}
+						else if (blue_flag) {
+							blue_x -= dx[i], blue_y -= dy[i];
+							blue_flag = false;
+						}
 					}
 				}
+
+				if (map[blue_x + dx[i]][blue_y + dy[i]] == 'O')
+					continue;
+
+				if (map[red_x + dx[i]][red_y + dy[i]] == 'O') {
+					ans = turn;
+					goal_flag = true;
+					break;
+				}
+
+				q.push({ i, 1000 * red_x + 100 * red_y + 10 * blue_x + blue_y });
 			}
-
-			//if (red_visit[red_x][red_y] && blue_visit[blue_x][blue_y]) continue;
-
-			//if (0 < dist[red_x][red_y] && dist[red_x][red_y] <= dist[curr_red[0]][curr_red[1]] + 1) continue;
-
-			//red_visit[red_x][red_y] = 1;
-			//blue_visit[blue_x][blue_y] = 1;
-			dist[red_x][red_y] = dist[curr_red[0]][curr_red[1]] + 1;
-
-			if (map[blue_x + dx[i]][blue_y + dy[i]] == 'O')
-				continue;
-
-			if (map[red_x + dx[i]][red_y + dy[i]] == 'O') {
-				ans = dist[red_x][red_y];
-				goal_flag = true;
-				break;
-			}
-
-			q_red.push({ red_x, red_y });
-			q_blue.push({ blue_x, blue_y });
+			if (goal_flag) break;
+			q_sz -= 1;
 		}
 		if (goal_flag) break;
-
-		//turn += 1;
+		turn += 1;
 	}
 
 	cout << ans << '\n';
-
 }
 
 int main() {
@@ -105,20 +87,23 @@ int main() {
 
 	cin >> n >> m;
 
+	int temp = 0;
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> map[i][j];
 
 			if (map[i][j] == 'R') {
 				map[i][j] = '.';
-				q_red.push({ i, j });
+				temp += 1000 * i + 100 * j;
 			}
 			else if (map[i][j] == 'B') {
 				map[i][j] = '.';
-				q_blue.push({ i, j });
+				temp += 10 * i + j;
 			}
 		}
 	}
+	q.push({ -1, temp });
 
 	solve();
 
