7f0129b1f8c6429019be0ac9dda0bc1a73737540
sojungoh <sojung3514@gmail.com>
2022-09-19 21:25:49 +0900

    solved[g5]: 백준 12865 평범한 배낭

--- c++/b-12865.cpp
+++ c++/b-12865.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int n, k, ans = 0;
-vector<pair<int, int>> product;
-
-void solve(int sz, int start, int value) {
-	if (sz) {
-		ans = max(ans, value);
-		return;
-	}
-
-	for (int i = start; i < n; i++) {
-
-	}
-
-}
+int dp[101][100001] = { 0, }, w[101], v[101];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
+	int n, k;
 	cin >> n >> k;
 
-	for (int i = 0; i < n; i++) {
-		int w, v;
-		cin >> w >> v;
-		product.push_back({ w, v });
+	for (int i = 1; i <= n; i++)
+		cin >> w[i] >> v[i];
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j - w[i] >= 0)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+			else dp[i][j] = dp[i - 1][j];
+		}
 	}
 
-	sort(product.begin(), product.end());
-
-	for(int i = 1; i <= n; i++)
-		solve(i, 0, 0);
+	cout << dp[n][k];
 
 	return 0;
 }
