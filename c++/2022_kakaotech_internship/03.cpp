97404bcfd1c92928032db8d59aaa98c7e2882953
sojungoh <sojung3514@gmail.com>
2023-02-03 18:10:21 +0900

    solved[Lv.3]: 2022 KAKAO TECH INTERNSHIP 코딩테스트 P3

--- c++/2022_kakaotech_internship/03.cpp
+++ c++/2022_kakaotech_internship/03.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[151][151];
+int dp[181][181];
 
 int solution(int alp, int cop, vector<vector<int>> problems) {
-	int answer = 0;
-	int max_alp = 0, max_cop = 0;
+	int answer = 0, max_alp = 0, max_cop = 0;
 	
 	// find the largest alp and cop
 	for(vector<int> vec : problems) {
@@ -13,22 +12,28 @@ int solution(int alp, int cop, vector<vector<int>> problems) {
 		max_cop = max(max_cop, vec[1]);
 	}
 	
-	// init dp array
-	for(int i = 0; i <= max_alp; ++i) {
-		for(int j = 0; j <= max_cop; ++j) {
-			if(i <= alp && j <= cop) dp[i][j] = 0;
+	alp = min(alp, max_alp);
+	cop = min(cop, max_cop);
+	
+	// initalize dp array
+	for(int i = 0; i <= 180; ++i) {
+		for(int j = 0; j <= 180; ++j) {
+			if (i <= alp && j <= cop) dp[i][j] = 0;
 			else dp[i][j] = 1e9;
 		}
 	}
 	
 	// dp
 	int alp_req, cop_req, alp_rwd, cop_rwd, cost;
+	int alp_idx, cop_idx;
 	
-	for(int i = 0; i <= max_alp; ++i) {
-		for(int j = 0; j <= max_cop; ++j) {
+	for(int i = alp; i <= max_alp; ++i) {
+		for(int j = cop; j <= max_cop; ++j) {
 			
-			if(alp <= i && cop <= j)
-				dp[i][j] = min(dp[i][j], dp[alp][cop] + (i - alp) + (j - cop));
+			alp_idx = min(i + 1, max_alp);
+			cop_idx = min(j + 1, max_cop);
+			dp[alp_idx][j] = min(dp[alp_idx][j], dp[i][j] + 1);
+			dp[i][cop_idx] = min(dp[i][cop_idx], dp[i][j] + 1);
 			
 			for(unsigned long k = 0; k < problems.size(); ++k) {
 				alp_req = problems[k][0];
@@ -38,7 +43,10 @@ int solution(int alp, int cop, vector<vector<int>> problems) {
 				cost = problems[k][4];
 				
 				if(i < alp_req || j < cop_req) continue;
-				else dp[i + alp_rwd][j + cop_rwd] = min(dp[i + alp_rwd][j + cop_rwd], dp[i][j] + cost);	
+				
+				alp_idx = min(i + alp_rwd, max_alp);
+				cop_idx = min(j + cop_rwd, max_cop);
+				dp[alp_idx][cop_idx] = min(dp[alp_idx][cop_idx], dp[i][j] + cost);	
 			}
 		}
 	}
@@ -52,17 +60,16 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	vector<vector<int>> problems = {{0, 0, 2, 1, 2}, {4, 5, 3, 1, 2}, 
-									{4, 11, 4, 0, 2}, {10, 4, 0, 4, 2}};
+	vector<vector<int>> problems = {{10, 15, 2, 1, 2}, {20, 20, 3, 3, 4}};
+		
+	cout << solution(20, 20, problems) << '\n';
 	
-	cout << solution(0, 0, problems) << '\n';
-	
-	for(int i = 0; i <= 10; ++i) {
-		for(int j = 0; j <= 11; ++j) {
-			cout << dp[i][j] << ' ';
-		}
-		cout << '\n';
-	}
+	//for(int i = 0; i <= 10; ++i) {
+		//for(int j = 0; j <= 11; ++j) {
+			//cout << dp[i][j] << ' ';
+		//}
+		//cout << '\n';
+	//}
 	
 	return 0;
 }
