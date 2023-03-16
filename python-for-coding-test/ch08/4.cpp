d257c7cbdb76741fa6f983baa3c89656d90326e6
sojungoh <sojung3514@gmail.com>
2023-03-16 23:19:49 +0900

    실전문제: 바닥 공사

--- python-for-coding-test/ch08/4.cpp
+++ python-for-coding-test/ch08/4.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -10,10 +9,11 @@ void solve(int n) {
 	dp[1] = 1;
 	dp[2] = 3;
 	
+	// 나눗셈을 미리 해야 int 범위 초과 x
 	for(int i = 3; i <= n; ++i)
-		dp[i] = dp[i - 1] + dp[i - 2] * 2;
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 796796;
 	
-	cout << dp[n] % 796796 << endl;
+	cout << dp[n] << endl;
 }
 
 int main() {
@@ -21,7 +21,6 @@ int main() {
 	cin.tie(0); cout.tie(0);
 	
 	int n;
-	cin >> n;
 	
 	solve(n);
 	
