b3307662967bcf4d68b16b18e7b90dee8d040e1a
sojungoh <sojung3514@gmail.com>
2023-03-25 21:11:00 +0900

    solved[s3]: 2xn 타일링

--- baekjoon/b-11726.cpp
+++ baekjoon/b-11726.cpp
@@ -9,10 +9,9 @@ void solve(int n) {
 	
 	dp[1] = 1;
 	dp[2] = 2;
-	dp[3] = 3;
 	
-	for(int i = 4; i <= n; ++i)
-		dp[i] = (dp[i - 1] * 2) % 10007;
+	for(int i = 3; i <= n; ++i)
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 	
 	cout << dp[n] << endl;
 }
