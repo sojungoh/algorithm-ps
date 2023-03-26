caca98c99956ec199a09bcc83c680f156dcb676f
sojungoh <sojung3514@gmail.com>
2023-03-26 17:48:23 +0900

    solved[s2]: 가장 긴 증가하는 부분 수열

---
+++ baekjoon/b-11053.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int arr[1000], dp[1000];
+
+void solve(int n) {
+	
+	int ans = 0;
+	
+	for(int i = 0; i < n; ++i)
+		dp[i] = 1;
+	
+	for(int i = 1; i < n; ++i)
+		for(int j = 0; j < i; ++j)
+			if(arr[j] < arr[i])
+				dp[i] = max(dp[i], dp[j] + 1);
+				
+	for(int i = 0; i < n; ++i)
+		ans = max(ans, dp[i]);
+	
+	cout << ans << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	for(int i = 0; i < n; ++i)
+		cin >> arr[i];
+	
+	solve(n);
+	
+	return 0;
+}
