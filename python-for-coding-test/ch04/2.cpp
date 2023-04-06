b2bd81b652fc50190f6dec790ea51ecc1af14c3f
sojungoh <sojung3514@gmail.com>
2023-04-06 23:22:12 +0900

    solving: 실전문제: 왕실의 나이트

---
+++ python-for-coding-test/ch04/2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1}; // n w s e
+
+void solve(string s) {
+	
+	int col = s[0] - 96;
+	int row = s[1] - '0';
+	
+	
+	for(int i = 0; i < 4; ++i) {
+		
+		int x, y;
+		
+		
+		
+	}
+	
+	
+	
+	
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	string s;
+	
+	cin >> s;
+	
+	solve(s);
+	
+	return 0;
+}
