3820638e800542fbf3f07df8133e998294f13c68
sojungoh <sojung3514@gmail.com>
2023-04-07 17:00:02 +0900

    실전문제: 왕실의나이트

--- python-for-coding-test/ch04/2.cpp
+++ python-for-coding-test/ch04/2.cpp
@@ -3,24 +3,32 @@
 
 using namespace std;
 
-int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1}; // n w s e
+int dx[4] = {0, -1, 0, 1}, dy[4] = {-1, 0, 1, 0}; // n w s e
 
 void solve(string s) {
-	
+		
+	int x, y, ans = 0;
 	int col = s[0] - 96;
 	int row = s[1] - '0';
 	
-	
 	for(int i = 0; i < 4; ++i) {
-		
-		int x, y;
-		
-		
-		
+		x = col + dx[i] * 2;
+		y = row + dy[i] * 2;
+			
+		for(int j = 0; j < 2; ++j) {
+			if(i % 2 == 0)
+				x += dx[2*j + 1];
+			else
+				y += dy[2*j];
+			
+			if(x < 1 || y < 1 || x > 8 || y > 8)
+				continue;
+			
+			ans += 1;
+		}
 	}
 	
-	
-	
+	cout << ans << endl;
 	
 }
 
