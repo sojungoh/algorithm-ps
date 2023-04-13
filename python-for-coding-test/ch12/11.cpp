fbebd379211633772fd520ba35b0fc7f2f63021a
sojungoh <sojung3514@gmail.com>
2023-04-13 21:22:51 +0900

    solved: Q11 뱀

--- python-for-coding-test/ch12/11.cpp
+++ python-for-coding-test/ch12/11.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
 int map[101][101] = {0, };
-int dx[4] = {0, -1, 0, 1}, dy[4] = {-1, 0, 1, 0}; // n w s e 
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1}; 
 
-queue<pair<int, char>> snake_info;
+vector<pair<int, char>> snake_info;
 
 void solve(int n) {
 	
-	int curr_x = 1, curr_y = 1, dir = 3;
-	int time = 0;
+	int h_time = 0, t_time = 0, h_idx = 0, t_idx = 0;
+	int head_info[3] = {1, 1, 3};
+	int tail_info[3] = {1, 1, 3};
 	
-	map[curr_x][curr_y] = 1;
+	map[1][1] = 1;
 	
 	while(true) {
 		
-		int x = curr_x + dx[dir];
-		int y = curr_y + dy[dir];
+		int x = head_info[0] + dx[head_info[2]];
+		int y = head_info[1] + dy[head_info[2]];
 		
 		if(x < 1 || y < 1 || x > n || y > n)
 			break;
@@ -26,26 +27,43 @@ void solve(int n) {
 		if(map[x][y] == 1)
 			break;
 		
-		if(map[x][y] != 2)
-			map[curr_x][curr_y] = 0;
+		if(map[x][y] != 2) {
+			map[tail_info[0]][tail_info[1]] = 0;
+			tail_info[0] += dx[tail_info[2]];	
+			tail_info[1] += dy[tail_info[2]];
+			t_time += 1;
+		}
 		
 		map[x][y] = 1;
 		
-		curr_x = x;
-		curr_y = y;
-		time += 1;
+		head_info[0] = x;
+		head_info[1] = y;
+		h_time += 1;
 		
-		if(!snake_info.empty() && time == snake_info.front().first) {
-			if(snake_info.front().second == 'L')
-				dir = (dir == 3) ? 0 : dir + 1;
-			else
-				dir = (dir == 0) ? 3 : dir - 1;
-			
-			snake_info.pop();
+		if(h_idx < snake_info.size()) {
+			if(h_time == snake_info[h_idx].first) {
+				if(snake_info[h_idx].second == 'L')
+					head_info[2] = (head_info[2] == 3) ? 0 : head_info[2] + 1;
+				else
+					head_info[2] = (head_info[2] == 0) ? 3 : head_info[2] - 1;
+				
+				h_idx += 1;
+			}
+		}
+		
+		if(t_idx < snake_info.size()) {
+			if(t_time == snake_info[t_idx].first) {
+				if(snake_info[t_idx].second == 'L')
+					tail_info[2] = (tail_info[2] == 3) ? 0 : tail_info[2] + 1;
+				else
+					tail_info[2] = (tail_info[2] == 0) ? 3 : tail_info[2] - 1;
+					
+				t_idx += 1;
+			}
 		}
 	}
 	
-	cout << time + 1 << endl;
+	cout << h_time + 1 << endl;
 }
 
 int main() {
@@ -67,7 +85,7 @@ int main() {
 	char c;
 	for(int i = 0; i < l; ++i) {
 		cin >> x >> c;
-		snake_info.push({x, c});
+		snake_info.push_back({x, c});
 	}
 	
 	solve(n);
