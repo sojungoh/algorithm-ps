8bf12115b59ece770e52ad36c5022d20d78bf6cf
sojungoh <sojung3514@gmail.com>
2023-04-23 20:17:36 +0900

    solving: Q20 감시 피하기

---
+++ python-for-coding-test/ch13/20.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n; 
+char map[7][7], visited[7][7] = {0, };
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
+vector<pair<int, int>> t_coord, o_coord;
+
+bool simulate() {
+	bool flag = false;
+	
+	for(pair<int, int> p : o_coord)
+		map[p.first][p.second] = 'O';
+	
+	for(pair<int, int> p : t_coord) {
+		int nx = p.first;
+		int ny = p.second;
+		
+		for(int i = 0; i < 4; ++i) {
+			while(true) {
+				nx += dx[i];
+				ny += dy[i];
+				
+				if(nx < 1 || ny < 1 || nx > n || ny > n)
+					break;
+				
+				if(map[nx][ny] != 'X')
+					break;
+				
+				if(map[nx][ny] == 'S') {
+					flag = true;
+					break;
+				}
+			}
+			
+			if(flag)
+				break;
+		}
+		
+		if(flag)
+			break;
+	}
+	
+	for(pair<int, int> p : o_coord)
+		map[p.first][p.second] = 'X';
+	
+	return flag;
+}
+
+bool solve(int x, int y) {
+	
+	if(o_coord.size() == 3) {
+		return simulate();
+	}
+	
+	for(int i = x; i <= n; ++i) {
+		for(int j = y; j <= n; ++j) {
+			
+			if(visited[i][j])
+				continue;
+			
+			visited[i][j] = 1;
+			o_coord.push_back({i, j});
+			
+			bool flag = solve(i + 1, j + 1);
+			
+			if(!flag)
+				return flag;
+			
+			visited[i][j] = 0;
+			o_coord.pop_back();
+		}
+	}
+	
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> n;
+	for(int i = 1; i <= n; ++i) {
+		for(int j = 1; j <= n; ++j) {
+			cin >> map[i][j];
+			
+			if(map[i][j] == 'T')
+				t_coord.push_back({i, j});
+		}
+	}
+	
+	bool flag = solve(0, 0);
+	
+	if(flag)
+		cout << "NO" << endl;
+	else
+		cout << "YES" << endl;
+	
+	return 0;
+}
