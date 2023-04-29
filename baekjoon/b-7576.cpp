d94ffe20ccf519b59c8ec13367d8f2cf4968822e
sojungoh <sojung3514@gmail.com>
2023-04-29 23:50:02 +0900

    solving[g5]: 백준 7576 토마토

---
+++ baekjoon/b-7576.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m, map[1000][1000];
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
+vector<pair<int, int>> tomato_coord;
+
+void bfs() {
+	
+	queue<pair<int, int>> q;
+	
+	for(pair<int, int> p : tomato_coord)
+		q.push({p.first, p.second});
+	
+	while(!q.empty()) {
+		
+		size_t sz = q.size();
+		
+		for(int i = 0; i < sz; ++i) {
+		
+			int x = q.front().first;
+			int y = q.front().second;
+		
+			q.pop();
+		
+			for(int d = 0; d < 4; ++d) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+			
+				if(nx < 0 || ny < 0 || nx >= n || ny >= m)
+					continue;
+			
+				if(map[nx][ny] != 0)
+					continue;
+			
+				map[nx][ny] = 1;
+			
+				q.push({nx, ny});
+			}
+		}
+	}
+}
+
+int solve() {
+	
+	if(tomato_coord.size() == 0)
+		return -1;
+	if(tomato_coord.size() == n * m)
+		return 0;
+	
+	bfs();
+	
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; ++i) {
+		for(int j = 0; j < n; ++j) {
+			cin >> map[i][j];;
+			
+			if(map[i][j] == 1)
+				tomato_coord.push_back({i, j});
+		}
+	}
+	
+	cout << solve() << endl;
+	
+	return 0;
+}
