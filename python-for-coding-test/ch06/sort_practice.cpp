f7cb39334c819ccfecea7d584bc215de4bb2a9f0
sojungoh <sojung3514@gmail.com>
2023-05-02 19:42:27 +0900

    ch06 정렬: 선택 정렬

--- python-for-coding-test/ch06/sort_practice.cpp
+++ python-for-coding-test/ch06/sort_practice.cpp
@@ -7,16 +7,38 @@ void selection_sort(vector<int> origin) {
 	
 	vector<int> v = origin;
 	
-	for(int i = 0; i < v.size() - 1; ++i) {
+	for(size_t i = 0; i < v.size() - 1; ++i) {
 		int min_idx = i;
 		
-		for(int j = i + 1; j < v.size(); ++j)
+		for(size_t j = i + 1; j < v.size(); ++j)
 			if(v[j] < v[min_idx])
 				min_idx = j;
 			
 		int temp = v[i];
 		v[i] = v[min_idx];
 		v[min_idx] = temp;
+	}
+	
+	for(int num : v)
+		cout << num << ' ';
+	cout << endl;
+}
+
+void insertion_sort(vector<int> origin) {
+	
+	vector<int> v = origin;
+	
+	for(size_t i = 1; i < v.size(); ++i) {
+		int idx = i;
+		for(size_t j = i - 1; j >= 0; --j) {
+			if(v[idx] >= v[j])
+				break;
+			
+			int temp = v[idx];
+			v[idx] = v[j];
+			v[j] = temp;
+			idx = j;
+		}
 	}
 	
 	for(int num : v)
@@ -31,10 +53,7 @@ int main() {
 	vector<int> vec = {3, 39, 48, 10, 49, 12};
 	
 	selection_sort(vec);
-	
-	for(int num : vec)
-		cout << num << ' ';
-	cout << endl;
+	insertion_sort(vec);
 	
 	return 0;
 }
