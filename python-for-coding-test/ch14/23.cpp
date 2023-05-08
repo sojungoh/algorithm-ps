41f36275ca7f15fc8f1abac02f4729852f5413de
sojungoh <sojung3514@gmail.com>
2023-05-08 20:12:33 +0900

    solved: Q23 국영수

--- python-for-coding-test/ch14/23.cpp
+++ python-for-coding-test/ch14/23.cpp
@@ -7,48 +7,49 @@ using namespace std;
 
 int n, score[100000][3];
 vector<string> name;
+vector<string> ans;
 
 void sort_by_score(vector<int> v, int order) {
 	
 	if(order == 3) {
-		vector<string> s3;
+		vector<string> name_vec;
 		
 		for(int idx : v)
-			s3.push_back(name[idx]);
+			name_vec.push_back(name[idx]);
 		
-		sort(s3.begin(), s3.end());
+		sort(name_vec.begin(), name_vec.end());
 		
-		for(string s : s3)
-			cout << s << endl;
+		for(string s : name_vec)
+			ans.push_back(s);
 		
 		return;
 	}
 	
-	vector<vector<int>> s(101);
+	vector<vector<int>> sort_vec(101);
 	
 	for(int idx : v)
-		s[score[idx][order]].push_back(idx);
+		sort_vec[score[idx][order]].push_back(idx);
 	
 	if(order == 1) {
 		for(int i = 1; i <= 100; ++i) {
-			if(s[i].size() == 0)
+			if(sort_vec[i].size() == 0)
 				continue;
 			
-			if(s[i].size() == 1)
-				cout << name[i] << endl;
+			if(sort_vec[i].size() == 1)
+				ans.push_back(name[sort_vec[i][0]]);
 			else
-				sort_by_score(s[i], order + 1);
+				sort_by_score(sort_vec[i], order + 1);
 		}
 	}
 	else {
 		for(int i = 100; i > 0; --i) {
-			if(s[i].size() == 0)
+			if(sort_vec[i].size() == 0)
 				continue;
 			
-			if(s[i].size() == 1)
-				cout << name[i] << endl;
+			if(sort_vec[i].size() == 1)
+				ans.push_back(name[sort_vec[i][0]]);
 			else
-				sort_by_score(s[i], order + 1);
+				sort_by_score(sort_vec[i], order + 1);
 		}
 	}
 }
@@ -57,7 +58,7 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	vector<vector<int>> s1(101);
+	vector<vector<int>> sort_vec(101);
 	string s;
 	
 	cin >> n;
@@ -65,18 +66,21 @@ int main() {
 	for(int i = 0; i < n; ++i) {
 		cin >> s >> score[i][0] >> score[i][1] >> score[i][2];
 		name.push_back(s);
-		s1[score[i][0]].push_back(i);
+		sort_vec[score[i][0]].push_back(i);
 	}
 	
 	for(int i = 100; i > 0; --i) {
-		if(s1[i].size() == 0)
+		if(sort_vec[i].size() == 0)
 			continue;
 		
-		if(s1[i].size() == 1)
-			cout << name[i] << endl;
+		if(sort_vec[i].size() == 1)
+			ans.push_back(name[sort_vec[i][0]]);
 		else
-			sort_by_score(s1[i], 1);
+			sort_by_score(sort_vec[i], 1);
 	}
-		
+	
+	for(string a: ans)
+		cout << a << '\n'; //endl -> timeout occurs!!
+		 
 	return 0;
 }
