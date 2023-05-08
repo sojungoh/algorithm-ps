780a5b7522c4102555cf8bd85ec702ad2d277f30
sojungoh <sojung3514@gmail.com>
2023-05-08 17:03:32 +0900

    solving: Q23 국영수

--- python-for-coding-test/ch14/23.cpp
+++ python-for-coding-test/ch14/23.cpp
@@ -1,100 +1,82 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int n, score[100000][3];
 vector<string> name;
 
-void solve() {
+void sort_by_score(vector<int> v, int order) {
 	
-	for(int i = 1; i < n; ++i) {
-		for(int j = i; j >= 0; --j) {
-			if(score[j][0] < score[j - 1][0])
-				break;
-			
-			if(score[j][0] > score[j - 1][0]) {
-				for(int k = 0; k < 3; ++k) {
-					int temp = score[j][k];
-					score[j][k] = score[j - 1][k];
-					score[j - 1][k] = temp;
-				}
-				
-				string s = name[j];
-				name[j] = name[j - 1];
-				name[j - 1] = s;
-				
-				continue;
-			}
-			
-			if(score[j][1] > score[j - 1][1])
-				continue;
-			
-			if(score[j][1] < score[j - 1][1]) {
-				for(int k = 0; k < 3; ++k) {
-					int temp = score[j][k];
-					score[j][k] = score[j - 1][k];
-					score[j - 1][k] = temp;
-				}
-				
-				string s = name[j];
-				name[j] = name[j - 1];
-				name[j - 1] = s;
-				
-				continue;
-			}
-			
-			if(score[j][2] < score[j - 1][2])
-				continue;
-			
-			if(score[j][2] > score[j - 1][2]) {
-				for(int k = 0; k < 3; ++k) {
-					int temp = score[j][k];
-					score[j][k] = score[j - 1][k];
-					score[j - 1][k] = temp;
-				}
-				
-				string s = name[j];
-				name[j] = name[j - 1];
-				name[j - 1] = s;
-				
-				continue;
-			}
-			
-			if(name[j] > name[j - 1])
-				continue;
-			
-			for(int k = 0; k < 3; ++k) {
-				int temp = score[j][k];
-				score[j][k] = score[j - 1][k];
-				score[j - 1][k] = temp;
-			}
-			
-			string s = name[j];
-			name[j] = name[j - 1];
-			name[j - 1] = s;
-		}
+	if(order == 3) {
+		vector<string> s3;
+		
+		for(int idx : v)
+			s3.push_back(name[idx]);
+		
+		sort(s3.begin(), s3.end());
+		
+		for(string s : s3)
+			cout << s << endl;
+		
+		return;
 	}
 	
-	for(string s : name)
-		cout << s << endl;
+	vector<vector<int>> s(101);
 	
+	for(int idx : v)
+		s[score[idx][order]].push_back(idx);
+	
+	if(order == 1) {
+		for(int i = 1; i <= 100; ++i) {
+			if(s[i].size() == 0)
+				continue;
+			
+			if(s[i].size() == 1)
+				cout << name[i] << endl;
+			else
+				sort_by_score(s[i], order + 1);
+		}
+	}
+	else {
+		for(int i = 100; i > 0; --i) {
+			if(s[i].size() == 0)
+				continue;
+			
+			if(s[i].size() == 1)
+				cout << name[i] << endl;
+			else
+				sort_by_score(s[i], order + 1);
+		}
+	}
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	cin >> n;
-	
+	vector<vector<int>> s1(101);
 	string s;
+	
+	cin >> n;
+
 	for(int i = 0; i < n; ++i) {
 		cin >> s >> score[i][0] >> score[i][1] >> score[i][2];
 		name.push_back(s);
+		s1[score[i][0]].push_back(i);
 	}
 	
-	solve();
+	for(int i = 100; i > 0; --i) {
+		if(s1[i].size() == 0)
+			continue;
+		
+		if(s1[i].size() == 1)
+			cout << name[i] << endl;
+		else
+			sort_by_score(s1[i], 1);
+	}
 		
 	return 0;
 }
