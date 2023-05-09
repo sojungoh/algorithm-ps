7082fe3bc6a277323aa6620f975b35ccbf7a1fbc
sojungoh <sojung3514@gmail.com>
2023-05-09 22:25:10 +0900

    solving: Q24 안테나

---
+++ python-for-coding-test/ch14/24.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	int t;
+	for(int i = 0; i < n; ++i){
+		cin >> t;
+		v,push_back(t);
+	}
+	
+	
+	
+	
+	return 0;
+}
