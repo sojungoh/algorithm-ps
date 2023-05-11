2f8fbd3b753ea80d3613f9b842e8755dd7e3bd7e
sojungoh <sojung3514@gmail.com>
2023-05-11 22:03:07 +0900

    solving: Q26 카드 정렬하기

--- python-for-coding-test/ch14/26.cpp
+++ python-for-coding-test/ch14/26.cpp
@@ -8,7 +8,8 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	int n, ans = 0;
+	int n;
+	int max = 0, ans = 0;
 	
 	cin >> n;
 	
@@ -16,25 +17,30 @@ int main() {
 	for(int i = 0; i < n; ++i) {
 		cin >> a;
 		count[a] += 1;
+		if(max < a)
+			max = a;
 	}
 	
-	int sum = 0;
+	int first = 0, check = 0;
 	for(int i = 0; i <= 1000; ++i) {
-		if(count[i] == 0)
-			continue;
-		
 		int num = count[i];
 		
 		while(num > 0) {
-			if(!sum) {
-				sum = i;
+			if(!first) {
+				first = i;
 				num -= 1;
 				continue;
 			}
 			
 			num -= 1;
-			sum += i;
-			ans += sum;
+			check += 1;
+			ans += first + i;
+			
+			// what if sum overflows 1000?
+			if(check != n - 1)
+				count[first + i] += 1;
+			
+			first = 0;
 		}
 	}
 	
