780bf9e70bc1183cc1521eddd0a6932db049e762
sojungoh <sojung3514@gmail.com>
2023-05-12 11:25:14 +0900

    solved: Q26 카드 정렬하기

--- python-for-coding-test/ch14/26.cpp
+++ python-for-coding-test/ch14/26.cpp
@@ -1,44 +1,47 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-int count[1001] = {0, };
+priority_queue<int> pq;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	int n;
-	int max = 0, ans = 0;
+	int n, ans = 0;
 	
 	cin >> n;
 	
 	int a;
 	for(int i = 0; i < n; ++i) {
 		cin >> a;
-		count[a] += 1;
-		if(max < a)
-			max = a;
+		pq.push(-a);
 	}
 	
-	int first = 0, check = 0;
-	for(int i = 0; i <= 1000; ++i) {
-		int num = count[i];
+	if(pq.size() < 2) {
+		ans = 0;
+	}
+	else {	
+		for(int i = 0; i < 2; ++i) {
+			ans += -pq.top();
+			pq.pop();
+		}
+	
+		pq.push(-ans);
 		
-		while(num > 0) {
+		int first = 0;
+		while(!pq.empty()) {
 			if(!first) {
-				first = i;
-				num -= 1;
+				first = -pq.top();
+				pq.pop();
 				continue;
 			}
 			
-			num -= 1;
-			check += 1;
-			ans += first + i;
-			
-			// what if sum overflows 1000?
-			if(check != n - 1)
-				count[first + i] += 1;
+			int sum = first - pq.top();
+			ans += sum;
+			pq.pop();
+			pq.push(-sum);
 			
 			first = 0;
 		}
