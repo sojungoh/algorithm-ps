d6331b46e2a72ea777cebf40d42bb86b2901e813
sojungoh <sojung3514@gmail.com>
2023-05-25 23:42:36 +0900

    solving: Q29 공유기 설치

--- python-for-coding-test/ch15/29.cpp
+++ python-for-coding-test/ch15/29.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
+
+set<int> s;
+
+void binary_search(int start, int end) {
+	
+	while(start >= end) {
+		
+		int mid = (start + end) / 2;
+		
+		if(s.find(mid) != s.end()) {
+			
+			
+			
+		}
+		
+		
+	}
+	
+}
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -9,7 +29,13 @@ int main() {
 	int n, c;
 	cin >> n >> c;
 	
+	int in;
+	for(int i = 0; i < n; ++i) {
+		cin >> in;
+		s.push(in);
+	}
 	
+	binary_search(0, n - 1);
 	
 	return 0;
 }
