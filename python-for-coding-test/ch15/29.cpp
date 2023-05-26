0c2440bf6e972249b085cebd312dea15853b2d27
sojungoh <sojung3514@gmail.com>
2023-05-26 22:32:53 +0900

    solved: Q29 공유기 설치

--- python-for-coding-test/ch15/29.cpp
+++ python-for-coding-test/ch15/29.cpp
@@ -1,41 +1,58 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-set<int> s;
+vector<int> v;
+int n, c;
 
 void binary_search(int start, int end) {
 	
-	while(start >= end) {
+	int ans = 0;
+	
+	while(start <= end) {
 		
 		int mid = (start + end) / 2;
 		
-		if(s.find(mid) != s.end()) {
-			
-			
-			
+		int count = 1;
+		int val = v[0];
+		
+		for(int i = 1; i < v.size(); ++i) {
+			if(val + mid <= v[i]) {
+				count += 1;
+				val = v[i];
+			}
 		}
 		
-		
+		if(count >= c) {
+			start = mid + 1;
+			ans = mid;
+		}
+		else {
+			end = mid - 1;
+		}		
 	}
 	
+	cout << ans << '\n';
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	int n, c;
 	cin >> n >> c;
 	
 	int in;
 	for(int i = 0; i < n; ++i) {
 		cin >> in;
-		s.push(in);
+		v.push_back(in);
 	}
 	
-	binary_search(0, n - 1);
+	sort(v.begin(), v.end());
+	
+	binary_search(1, v[n - 1] - v[0]);
 	
 	return 0;
 }
