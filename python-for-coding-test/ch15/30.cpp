c18ca6cb4fb4c7bb339e9cdf00d970f4eaab9762
sojungoh <sojung3514@gmail.com>
2023-05-28 08:08:51 +0900

    solving: Q30 가사 검색

--- python-for-coding-test/ch15/30.cpp
+++ python-for-coding-test/ch15/30.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int match = 0;
+vector<int> word_index(10001);
 
 void binary_search(vector<string>& words, string query, int start, int end) {
     if(start > end)
@@ -29,10 +30,23 @@ vector<int> solution(vector<string> words, vector<string> queries) {
     vector<int> answer;
     size_t n = words.size() - 1;
     
-    sort(words.begin(), words.end());
+    for(int i = 0; i <= n; ++i) {
+        
+    }
     
+    int max_idx = -1, min_idx = n + 1;
     for(string query : queries) {
         match = 0;
+        
+        for(int i = 0; i < query.length(); ++i) {
+            if(c == '?') {
+                max_idx = max(max_idx, i);
+                min_idx = min(min_idx, i);
+            }
+        }
+        
+        
+        
         binary_search(words, query, 0, n);
         answer.push_back(match);
     }
