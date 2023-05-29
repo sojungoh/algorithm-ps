7ea51f5450db124bf97a8e2d3ebf091878100b7a
sojungoh <sojung3514@gmail.com>
2023-05-29 21:31:26 +0900

    solved: Q30 가사 검색 - 효율성 1, 2, 3 시간초과

--- python-for-coding-test/ch15/30.cpp
+++ python-for-coding-test/ch15/30.cpp
@@ -5,24 +5,24 @@
 using namespace std;
 
 int match = 0;
-vector<int> word_index(10001);
 
-void binary_search(vector<string>& words, string query, int start, int end) {
+void binary_search(vector<pair<string, int>>& words, string query, int length, int start, int end) {
     if(start > end)
         return;
     
     int mid = (start + end) / 2;
     
-    if(words[mid].compare(query) == 0) {
-        match += 1;
-        binary_search(words, query, mid + 1, end);
-        binary_search(words, query, start, mid - 1);
+    if(words[mid].first.compare(query) == 0) {
+        if(words[mid].second == length)
+            match += 1;
+        binary_search(words, query, length, mid + 1, end);
+        binary_search(words, query, length, start, mid - 1);   
     }
-    else if(words[mid].compare(query) < 0) {
-        binary_search(words, query, mid + 1, end);
+    else if(words[mid].first.compare(query) < 0) {
+        binary_search(words, query, length, mid + 1, end);
     }
     else {
-        binary_search(words, query, start, mid - 1);
+        binary_search(words, query, length, start, mid - 1);
     }
 }
 
@@ -30,24 +30,41 @@ vector<int> solution(vector<string> words, vector<string> queries) {
     vector<int> answer;
     size_t n = words.size() - 1;
     
-    for(int i = 0; i <= n; ++i) {
-        
-    }
-    
-    int max_idx = -1, min_idx = n + 1;
     for(string query : queries) {
-        match = 0;
+        size_t l = query.length();
+        int start_idx = l, end_idx = -1;
         
-        for(int i = 0; i < query.length(); ++i) {
-            if(c == '?') {
-                max_idx = max(max_idx, i);
-                min_idx = min(min_idx, i);
+        for(int i = 0; i < l; ++i) {
+            if(query[i] != '?') {
+                start_idx = min(start_idx, i);
+                end_idx = max(end_idx, i);
             }
         }
         
+        match = 0;
         
+        if(start_idx == l && end_idx == -1) {
+			for(string word: words) {
+				if(word.length() == l)
+					match += 1;
+			}
+			answer.push_back(match);
+			continue;
+		}
         
-        binary_search(words, query, 0, n);
+        vector<pair<string, int>> sub_words;
+        
+        for(string word : words) {
+            if(start_idx < word.length() && end_idx < word.length()) {
+                string s = word.substr(start_idx, end_idx + 1);
+                sub_words.push_back({s, word.length()});   
+            }
+        }
+        
+        sort(sub_words.begin(), sub_words.end());
+        
+        binary_search(sub_words, query.substr(start_idx, end_idx + 1), l, 0, sub_words.size() - 1);
+        
         answer.push_back(match);
     }
     
