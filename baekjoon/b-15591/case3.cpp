39ed0f8da3b38a07d9790b8768ad3eaa076a4f6e
sojungoh <sojung3514@gmail.com>
2023-07-25 16:11:30 +0900

    bfs - 문제 조건 활용해 시간 단축

--- baekjoon/b-15591/case3.cpp
+++ baekjoon/b-15591/case3.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
 using pii = pair<int, int>;
 
 int N, Q;
+
+int bfs(vector<vector<pii>>& g, int k, int v) {
+    queue<int> que;
+    que.push(v);
+    vector<bool> visited(N + 1, false);
+
+    int ans = -1;
+    while(!que.empty()) {
+        int curr = que.front();
+        que.pop();
+
+        if(visited[curr])
+            continue;
+        visited[curr] = true;
+        ans += 1;
+
+        for(auto [q, r] : g[curr]) {
+            if(r < k)
+                continue;
+            que.push(q);
+        }
+    }
+    
+    return ans;
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -22,54 +46,15 @@ int main() {
         graph[q].push_back({p, r});
     }
 
-    vector<vector<pii>> query(N + 1);
+    vector<pii> query(Q);
     for(int i = 0; i < Q; ++i) {
         int k, v;
         cin >> k >> v;
-        query[v].push_back({k, i});
+        query[i] = {k, v};
     }
 
-    for(auto& vec : query)
-        sort(vec.rbegin(), vec.rend());
-
-    vector<int> ans(Q, 0);
-    for(int v = 1; v <= N; ++v) {
-        if(query[v].empty())
-            continue;
-        
-        vector<bool> visited(N + 1, false);
-        int sum = 0;
-        int checkpoint = v;
-        for(auto [k, i] : query[v]) {
-            ans[i] += sum;
-
-            queue<int> que;
-            que.push(checkpoint);
-            
-            sum = 0;
-            while(!que.empty()) {
-                int curr = que.front();
-                que.pop();
-
-                if(curr != checkpoint && visited[curr])
-                    continue;
-                visited[curr] = true;
-
-                for(auto [q, r] : graph[curr]) {
-                    if(r < k) {
-                        checkpoint = curr;
-                        continue;
-                    }
-                    sum += 1;
-                    que.push(q);
-                }
-            }
-            ans[i] += sum;
-        }
-    }
-
-    for(int a : ans)
-        cout << a << '\n';
-
+    for(auto [k, v] : query)
+        cout << bfs(graph, k, v) << '\n';
+    
     return 0;
 }
