2f4025c0f206b5101cc6ed8512e627a31e85c6ab
sojungoh <sojung3514@gmail.com>
2023-08-10 17:46:57 +0900

    solved[g1]: 백준 1800 인터넷 설치

--- baekjoon/b-1800/sol1.cpp
+++ baekjoon/b-1800/sol1.cpp
@@ -6,48 +6,33 @@
 using namespace std;
 
 int N, P, K;
+vector<vector<pair<int, int>>> cables;
 
-void solve(vector<vector<pair<int, int>>>& cables) {
-    int answer = -1;
-    vector<vector<int>> dst(N + 1, vector<int>(K + 1));
-
-    for(int i = 1; i <= N; ++i)
-        fill(dst[i].begin(), dst[i].end(), int(1e7));
+bool dijkstra(int x) {
+    vector<int> dist(N + 1);
+    fill(dist.begin(), dist.end(), N + 1);
     
-    for(int i = 0; i <= K; ++i) {
-        dst[1][i] = 0;
-    }
+    priority_queue<pair<int, int>> pq;
+    pq.push({0, 1});
 
-    queue<int> q;
-    q.push(1);
+    while(!pq.empty()) {
+        int cost = -pq.top().first;
+        int curr = pq.top().second;
+        pq.pop();
 
-    while(!q.empty()) {
-        int curr = q.front();
-        q.pop();
-
+        if(dist[curr] <= cost)
+            continue;
+        dist[curr] = cost;
+        
         for(auto p : cables[curr]) {
-            int next = p.first;
-            int cost = p.second;
-            bool push_flag = false;
-
-            // k = 0 일 때와 아닐 때
-            for(int i = 0; i <= K; ++i) {
-                int cmp = (i == 0) ? max(dst[curr][i], cost) : min(dst[curr][i - 1], cost);
-                if(dst[next][i] <= cmp)
-                    continue;
-                dst[next][i] = cmp;
-                push_flag = true;
-            }
-
-            if(push_flag)
-                q.push(next);
+            int cmp = cost + (p.second > x);
+            if(dist[p.first] <= cmp)
+                continue;
+            pq.push({-cmp, p.first});
         }
     }
 
-    if(dst[N][K] < int(1e7))
-        answer = dst[N][K];
-
-    cout << answer << '\n';
+    return dist[N] <= K;
 }
 
 int main() {
@@ -56,15 +41,39 @@ int main() {
 
     cin >> N >> P >> K;
     
-    vector<vector<pair<int, int>>> cables(N + 1); 
+    cables.resize(N + 1);
+
+    vector<int> cost;
     for(int i = 0; i < P; ++i) {
         int a, b, c;
         cin >> a >> b >> c;
         cables[a].push_back({b, c});
         cables[b].push_back({a, c});
+        cost.push_back(c);
     }
 
-    solve(cables);
+    sort(cost.begin(), cost.end());
+
+    int answer = -1;
+    int l = 0;
+    int r = N - 1;
+    while(l <= r) {
+        int mid = (l + r) / 2;
+        
+        if(dijkstra(cost[mid])) {
+            if(mid == 0) {
+                answer = 0;
+                break;
+            }
+            answer = cost[mid];
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    cout << answer << '\n';
     
     return 0;
 }
